21e4fc7992b9fac75647fe71e5f272dde81c80c8
Mayuge <mayuge.studio.public@gmail.com>
2023-10-29 10:05:35 +0900

    add: Add setup function

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
       console.log('DOM Loaded');
-      addClickListener(`.close`, hideModal);
+      setupModal();
       initializeGame();
       toggleScene(title);
     },
---
+++ js/setup.js
@@ -0,0 +1,3 @@
+function setupModal() {
+  addClickListener(`.close`, hideModal);
+}
